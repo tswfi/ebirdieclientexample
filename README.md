63fa03f4d8008928a446f0658fe821cde6609384
Tatu Wikman <tatu.wikman@gmail.com>
2021-04-29 18:53:01 +0300

    initial exmaple

---
+++ README.md
@@ -0,0 +1,15 @@
+# Simple example for using ebirdieclient
+
+Examples for using [ebirdieclient](https://github.com/tswfi/ebirdieclient) php library
+
+# Getting started
+
+```bash
+git clone git@github.com:tswfi/ebirdieclientexample.git
+composer install
+cp .env.example .env
+```
+
+And add your secrets to .env file (get these from eBirdie)
+
+Note: if you use the wsdl from staging the calls will be done against staging and if you use the live wsdl the calls will be done to live.
---
+++ clubservice_fetchAllClubs.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Example for fetching clubs from eBirdie
+ */
+require "vendor/autoload.php";
+
+# load secrets to env from .env
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+# init the client
+$client = new tswfi\Ebirdie\Client($_ENV['EBIRDIE_WSDL'], $_ENV['EBIRDIE_LOGIN'], $_ENV['EBIRDIE_PASS']);
+
+# fetch all clubs and print them out
+$clubs = $client->__soapCall('fetchAllClubs', []);
+
+foreach ($clubs as $club) {
+    print_r($club);
+}
